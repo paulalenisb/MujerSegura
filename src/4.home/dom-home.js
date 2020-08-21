3b6b43142a20d650692901050ca6565a5fa5deb3
AnaRativa09 <anarativa09@gmail.com>
2020-08-21 17:41:17 -0500

    Form login + styles v1

--- src/4.home/dom-home.js
+++ src/4.home/dom-home.js
@@ -6,19 +6,8 @@ export default () => {
   const divElement = document.createElement("div");
   divElement.innerHTML = view;
 
-  function initMap() {
-    
-    const options ={
-      zoom: 15,
-      location: {lat: 4.7110, lng: 74.0721}  
-    }
-    
-    //Mostrar el mapa de bogotá 
-    const map = new google.maps.Map(document.getElementById('map'), options);
-    
-    const marker = new google.maps.Marker({position: options.location, map: map});
+  
 
-  };
 
   return divElement;
 };
