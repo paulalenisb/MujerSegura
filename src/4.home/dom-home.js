59b56393318242d77d03471ccf29bff9b3ab12ae
TATIANA Toro <tatianatorog@gmail.com>
2020-08-21 15:55:37 -0500

    Update dom-home.js

--- src/4.home/dom-home.js
+++ src/4.home/dom-home.js
@@ -10,17 +10,13 @@ export default () => {
     
     const location  = {lat: 4.7110, lng: 74.0721};
     
-    var map = new google.maps.Map(
+    const map = new google.maps.Map(
         document.getElementById('map'), {zoom: 15, center: location}
         );
     
-    var marker = new google.maps.Marker({position: location, map: map});
-/*   }
+    const marker = new google.maps.Marker({position: location, map: map});
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 4.7110, lng: 74.0721},
-    zoom: 15; */
-  });
+  };
 
   return divElement;
 };
